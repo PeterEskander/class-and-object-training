dc58371bc5a339534dbd9c8898d694f7bcfe9328
Peter H Eskander <phe3@afsconnect2.njit.edu>
2018-03-07 23:34:35 -0500

    first and hopefully final commit

---
+++ pdo.php
@@ -0,0 +1,39 @@
+<?php
+	$username='phe3';
+	$password='Patoura01';
+	$hostname='sql2.njit.edu';
+	$dsn='mysql:host=sql2.njit.edu;dbname=localhost';
+	$username='phe3';
+	$password='Patoura01';
+echo 'Connected Successfully';
+	try{
+	   $db=new PDO($dsn, $username, $password);
+	} catch (PDOException $error) {
+            echo '<h3>DB Error </h3>';
+	    echo $error->getMessage();
+	    exit();
+	} catch (PDOException $error) {
+            echo '<h3>DB Error </h3>';
+	    echo $error->getMessage();
+	    exit();
+	}
+	$query='SELECT * FROM accounts WHERE id < :id';
+	$statement= $db->prepare($query);
+	$statement->bindvalue(':id', 6);
+	$statement->execute();
+	$accounts = $statement->fetchAll();
+echo "</br> ";
+	$counts=count($accounts);
+	echo "Results: $counts";
+?>
+<table border="1">
+<th> ID</th>
+<th> EMAIL </th>
+<?php foreach ($accounts as $account) : ?>
+<tr> 
+	<td><?php echo $account ['id']; ?></td>
+	<td><?php echo $account ['email']; ?></td>
+</tr>
+
+<?php endforeach; ?>
+</table>
